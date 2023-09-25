cfeb8124e19575109d726e5c3aa372000fc855d1
rvpmin <roxanaperezmedina6@gmail.com>
2023-09-25 13:23:26 -0600

    try excep implements

--- example.py
+++ example.py
@@ -2,7 +2,17 @@
 #by diianasuchiapa@gmail.com
 #GNU/GPL License
 import numpy as np
-x = np.pi 
-x=np.cos(x)
-print(x)
+data_list = np.arange(1, 11, 1)
+for x in data_list:
+    print("value: "+str(x))
+ 
+x="1"
+z=0
+try:
+    
+    z=int(x)+1
+    print("ok")
+except:
+    print("Error in sum of z.")
+print(z)
 
